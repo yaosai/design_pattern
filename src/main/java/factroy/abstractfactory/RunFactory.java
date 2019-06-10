8ff1ed685caf54f937711b3ad55c58bd0a03f00b
yaosai <yao.sai@hotmail.com>
2019-06-10 15:57:26 +0800

    完善抽象工厂

--- src/main/java/factroy/abstractfactory/RunFactory.java
+++ src/main/java/factroy/abstractfactory/RunFactory.java
@@ -2,6 +2,8 @@ package factroy.abstractfactory;
 
 import factroy.abstractfactory.product.Product;
 import factroy.abstractfactory.product.ProductX;
+import factroy.abstractfactory.product.ProductY;
+import factroy.abstractfactory.product.ProductZ;
 
 /**
  * 生产产品
@@ -10,9 +12,18 @@ import factroy.abstractfactory.product.ProductX;
  * @date 2019-06-10 15:24
  */
 public class RunFactory {
+    /**
+     * 需要什么类型的产品new就行了
+     *
+     * @author YaoS
+     * @date 19/6/10 15:55
+     */
     public static void main(String[] args) {
-        Product product = null;
-        product = new ProductX();
-        product.create();
+        Product productX = new ProductX();
+        productX.create();
+        Product productY = new ProductY();
+        productY.create();
+        Product productZ = new ProductZ();
+        productZ.create();
     }
 }
